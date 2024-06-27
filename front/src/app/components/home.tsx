5e92397f91067dff3dadc40acbf3edc3f600ce87
CarolinaRTC <carolinacarles@byronsolutions.com>
2024-06-27 18:27:49 -0300

    Ajustes
    home: botão quadrado, text-md, legenda fundo.
    footer: text-md.
    header: text-md.
    top5: text-md, itens do card centralizados.

--- front/src/app/components/home.tsx
+++ front/src/app/components/home.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 export const Home = () => {
   return (
-    <section id="home" className="pt-20 text-gray-100 sm:text-sm lg:text-xl">
+    <section id="home" className="pt-20 text-gray-100 text-md">
       <div className="flex flex-col justify-center items-center ">
         <img
           className="opacity-100"
           src=".\img\equalizador2.jpg"
-          alt="ESCOLHER LEGENDA"
+          alt="Fundo pixelizado em diversas cores formando colunas."
         />
         <div className="flex flex-col gap-5 absolute p-4">
           <div className="flex flex-col gap-5">
@@ -22,7 +22,7 @@ export const Home = () => {
           <div className="flex justify-start">
             <a
               href="#login"
-              className="flex justify-center bg-orange-500 px-4 py-1 rounded-full text-gray-100 hover:bg-orange-700 font-bold text-md transition hover:scale-110"
+              className="flex justify-center bg-orange-500 px-4 py-1 rounded text-gray-100 hover:bg-orange-700 font-bold text-md transition hover:scale-110"
             >
               Explorar
             </a>
--- front/src/app/components/top5.tsx
+++ front/src/app/components/top5.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Seção = () => {
   return (
-    <section id="top5" className=" bg-black p-10 sm:text-sm lg:text-xl">
+    <section id="top5" className=" bg-black p-10 text-md">
       <div className="flex flex-col gap-4 text-white">
         <h1 className="font-bold text-2xl">
           Top 5: Músicas mais tocadas no momento!
@@ -10,28 +10,48 @@ export const Seção = () => {
         <div className="flex gap-4">
           <div className=" bg-orange-500 rounded p-4 gap-2 flex flex-col">
             <h2 className="font-bold flex justify-center">Top 1</h2>
-            <img src=".\img\HGBC.png" alt="Foto do Álbum" className="w-2/4" />
-            <p>Nome da música</p>
+            <img
+              src=".\img\HGBC.png"
+              alt="Foto do Álbum"
+              className="w-2/4 inline-block m-auto"
+            />
+            <p className="flex justify-center">Nome da música</p>
           </div>
           <div className=" bg-orange-500 flex flex-col rounded p-4 gap-2">
-            <h2>Top 2</h2>
-            <img src=".\img\HGBC.png" alt="Foto do Álbum" className="w-2/4" />
-            <p>Nome da música</p>
+            <h2 className="font-bold flex justify-center">Top 2</h2>
+            <img
+              src=".\img\HGBC.png"
+              alt="Foto do Álbum"
+              className="w-2/4 inline-block m-auto"
+            />
+            <p className="flex justify-center">Nome da música</p>
           </div>
           <div className=" bg-orange-500 flex flex-col rounded p-4 gap-2">
-            <h2>Top 3</h2>
-            <img src=".\img\HGBC.png" alt="Foto do Álbum" className="w-2/4" />
-            <p>Nome da música</p>
+            <h2 className="font-bold flex justify-center">Top 3</h2>
+            <img
+              src=".\img\HGBC.png"
+              alt="Foto do Álbum"
+              className="w-2/4 inline-block m-auto"
+            />
+            <p className="flex justify-center">Nome da música</p>
           </div>
           <div className=" bg-orange-500 flex flex-col rounded p-4 gap-2">
-            <h2>Top 4</h2>
-            <img src=".\img\HGBC.png" alt="Foto do Álbum" className="w-2/4" />
-            <p>Nome da música</p>
+            <h2 className="font-bold flex justify-center">Top 4</h2>
+            <img
+              src=".\img\HGBC.png"
+              alt="Foto do Álbum"
+              className="w-2/4 inline-block m-auto"
+            />
+            <p className="flex justify-center">Nome da música</p>
           </div>
           <div className=" bg-orange-500 flex flex-col rounded p-4 gap-2">
-            <h2>Top 5</h2>
-            <img src=".\img\HGBC.png" alt="Foto do Álbum" className="w-2/4" />
-            <p>Nome da música</p>
+            <h2 className="font-bold flex justify-center">Top 5</h2>
+            <img
+              src=".\img\HGBC.png"
+              alt="Foto do Álbum"
+              className="w-2/4 inline-block m-auto"
+            />
+            <p className="flex justify-center">Nome da música</p>
           </div>
         </div>
       </div>
