e9ec9fec35e85ebe4dd03f890757c2507b5d8409
beatrizgnascimento <131934165+beatrizgnascimento@users.noreply.github.com>
2024-07-04 11:19:19 -0300

    Reorganizando tudo

--- front/src/app/components/musicas.tsx
+++ front/src/app/components/musicas.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const Música = () => {
+export const Musica = () => {
   return (
     <section className="bg-orange-700 text-gray-100 pt-20 text-md">
       <div className="flex flex-col p-20 gap-2">
